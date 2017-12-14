5d5b4ad3b977b46ea4a7ee87ed855d522a40b2ab
khanh <khanhvuquoc.1994@gmail.com>
2017-12-14 19:22:31 +0700

    cau hinh lai menu

--- src/app/config/menu-config.ts
+++ src/app/config/menu-config.ts
@@ -3,5 +3,5 @@ import { menu_option } from '../../app/models/menu_options'
 var item1 = new menu_option("Home","");
 var item2 = new menu_option("Maps","maps");
 var item3 = new menu_option("New Post","post");
-export const Menu_left = [item1,item2,item3];
+export const Menu_left = [item1,item2,item3]; 
         
